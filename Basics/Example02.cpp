239559cb4e4c98fed8c3bd4e25481691c933902b
WereWolfSeb <49239039+WereWolfSeb@users.noreply.github.com>
2019-04-03 08:31:11 -0500

    Create Example02.cpp

---
+++ Basics/Example02.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //////////////////02/////////////////
+    double d = 2.5;
+    int i = 2;
+    double d2 = d/i; // d2 == 1.25
+    int i2 = d/i; // i2 = 1
+    d2 = d/i; // d2 == 1.25
+    i2 = d/i; // i2 == 1
+    {
+        double dc;
+        cin >> dc;
+        double df = 9/5 * dc + 32
+    }
+    {
+        double dc;
+        cin >> dc;
+        double df = 9.0/5 * dc + 32
+    }
+
+    return 0;
+}
