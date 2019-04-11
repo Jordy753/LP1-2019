7964ffca963f90eabac0fdec4fdcb6483435442c
WereWolfSeb <49239039+WereWolfSeb@users.noreply.github.com>
2019-04-11 11:32:55 -0500

    Create Laboratorio13.cpp

---
+++ Basics/Laboratorio13.cpp
@@ -0,0 +1,14 @@
+    
+#include <iostream>
+using namespace std;
+
+int print_square(int v)
+{
+    cout << v << '\t' << v*v << endl;
+}
+
+int main()
+{
+    for (int i = 0; i < 100; ++i) print_square(i);
+    return 0;
+}
